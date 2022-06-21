ad40ecee2332b0ae37df35862694ea77eeb4cf94
Johannes van-den-Heuvel <vandenheu@cgpool1910.Informatik.Uni-Tuebingen.De>
2022-06-21 13:57:46 +0200

    reduced number of matrix inversions

--- distributions/gaussian.py
+++ distributions/gaussian.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 from dense import pack_dense, unpack_dense
 from .distribution import ExpDistribution
@@ -22,8 +21,6 @@ class Gaussian(ExpDistribution):
         """
         loc, scale = self.natural_to_standard()
 
-        assert scale.ndim == 3
-
         E_x = loc
         E_xxT = scale + torch.einsum("bi,bj->bij", (E_x, E_x))
         E_n = torch.ones(len(scale), device=self.device)
@@ -31,26 +28,38 @@ class Gaussian(ExpDistribution):
         return pack_dense(E_xxT, E_x, E_n, E_n)
 
     def logZ(self):
-        loc, scale = self.natural_to_standard()
+        """normalization constant
+        """
 
-        # TODO I don't understand why there is a (a+b) in the normalization constant.
-        _, _, a, b = unpack_dense(self.nat_param)
-
+        # USING STANDARD PARAMETERS (easier to understand)
+        # loc, scale = self.natural_to_standard()
         """
         We can do the following which is much more computationally efficient
             log det (scale) = 2 sum log diag (L)
         where L is the lower triangular matrix produced by Cholesky decomposition of scale (psd) matrix.
         """
-        L = torch.linalg.cholesky(scale)
+        # L = torch.linalg.cholesky(scale)
+        # value = (
+        #     2 * torch.sum(torch.log(torch.diagonal(L, dim1=-1, dim2=-2)), dim=-1)
+        #     + torch.bmm(
+        #         loc.unsqueeze(1), torch.bmm(torch.inverse(scale), loc[..., None])
+        #     ).squeeze()
+        #     + 2 * (a + b)
+        # )
+        # return 1 / 2 * torch.sum(value)
+
+        # USING NATURAL PARAMETERS (faster)
+        # TODO I don't understand why there is a (a+b) in the normalization constant.
+        eta_2, eta_1, a, b = unpack_dense(self.nat_param)
+
+        L = torch.linalg.cholesky(-2 * eta_2)
         value = (
-            2 * torch.sum(torch.log(torch.diagonal(L, dim1=-1, dim2=-2)), dim=-1)
-            + torch.bmm(
-                loc.unsqueeze(1), torch.bmm(torch.inverse(scale), loc[..., None])
-            ).squeeze()
-            + 2 * (a + b)
+            1 / 2 * torch.sum(eta_1 * torch.linalg.solve(-2 * eta_2, eta_1))
+            - torch.sum(torch.log(torch.diagonal(L, dim1=-1, dim2=-2)))
+            + torch.sum(a + b)
         )
 
-        return 1 / 2 * torch.sum(value)
+        return value
 
     def natural_to_standard(self):
         eta_2, eta_1, _, _ = unpack_dense(self.nat_param)
@@ -61,8 +70,9 @@ class Gaussian(ExpDistribution):
         return loc, scale
 
     def standard_to_natural(self, loc, scale):
-        eta_1 = torch.inverse(scale) @ loc
-        eta_2 = torch.flatten(-1 / 2 * torch.inverse(scale))
+        scale_inv = torch.inverse(scale)
+        eta_1 = scale_inv @ loc
+        eta_2 = torch.flatten(-1 / 2 * scale_inv)
 
         return pack_dense(eta_2, eta_1)
 
